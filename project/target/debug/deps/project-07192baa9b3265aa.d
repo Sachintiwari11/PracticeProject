24a0e4fcfb65b60b6e46d483d46d9945d1ed4b38
Sachin Tiwari <sachin.tiwari@uneecops.in>
2025-09-16 11:56:24 +0530

    new folder

---
+++ project/target/debug/deps/project-07192baa9b3265aa.d
@@ -0,0 +1,5 @@
+C:\Users\sachin.tiwari\Clonerust\PracticeProject\project\target\debug\deps\project-07192baa9b3265aa.d: src\main.rs
+
+C:\Users\sachin.tiwari\Clonerust\PracticeProject\project\target\debug\deps\libproject-07192baa9b3265aa.rmeta: src\main.rs
+
+src\main.rs:
